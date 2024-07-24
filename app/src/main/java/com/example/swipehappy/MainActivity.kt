f5dc1f4b6f2c9a29eb682c67a27f8978347fa9b6
able254 <muliable0110@gmail.com>
2024-07-24 11:51:48 +0300

    Added home page elements. No styling yet.

--- app/src/main/java/com/example/swipehappy/MainActivity.kt
+++ app/src/main/java/com/example/swipehappy/MainActivity.kt
@@ -4,44 +4,92 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.swipehappy.ui.theme.SwipeHappyTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
-            SwipeHappyTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+            loginDetails()
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun loginDetails() {
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    SwipeHappyTheme {
-        Greeting("Android")
+    //Variables to save user input
+
+    //Handles everything
+    Column {
+
+        //Handles Website logo section
+        Column {
+            Image(
+                painter = painterResource(id = R.drawable.logo),
+                contentDescription = "Swipe Happy logo",
+                modifier = Modifier
+                    .padding(10.dp)
+
+            )
+
+            //Welcoming texts and copy content
+            Text("Welcome to Swipe Happy. The premier dating app for young singles worldwide.")
+        }
+
+        //Handles user inputs and validation
+        Column {
+            //Prompt 1
+            Text("Enter email or phone number")
+
+            //variable for username field
+            var usernameText by remember {
+                mutableStateOf("")
+            }
+
+            //collect user input for username
+            TextField(
+                value = usernameText,
+                onValueChange = {usernameText = it}
+            )
+
+            //Prompt 2
+            Text("Enter password")
+
+            //Variable for password field
+            var passwordText by remember {
+                mutableStateOf("")
+            }
+
+            //collect user input for password
+            TextField(
+                value = passwordText,
+                onValueChange = {passwordText = it}
+            )
+
+            Button(onClick = { /*TODO*/ }) {
+                Text(text = "Login")
+            }
+
+            Button(onClick = { /*TODO*/ }) {
+                Text(text = "Register")
+            }
+        }
     }
+
 }
---
+++ app/src/main/java/com/example/swipehappy/dbHandler.kt
@@ -0,0 +1,4 @@
+package com.example.swipehappy
+
+class dbHandler {
+}
